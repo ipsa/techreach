e4584122a19a8cbeb7f49ccd8b67f7efd4f73f51
Tony Summerville <tonysummerville@gmail.com>
2009-02-17 16:24:34 -0600

    trying to get new user to work..

--- app/models/user_observer.rb
+++ app/models/user_observer.rb
@@ -1,6 +1,6 @@
 class UserObserver < ActiveRecord::Observer
   def after_create(user)
-    UserMailer.deliver_signup_notification(user) if user.not_using_openid?
+#    UserMailer.deliver_signup_notification(user) if user.not_using_openid?
   end
 
   def after_save(user)
